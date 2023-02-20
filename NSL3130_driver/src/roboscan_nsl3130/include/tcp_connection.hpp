5073301b4c0f0f597e03e1ade400001bd9204ff3
nano-roboscan <106071093+nano-roboscan@users.noreply.github.com>
2023-02-20 10:45:08 +0900

    Update tcp_connection.hpp
    NSL-3130AA

--- NSL3130_driver/src/roboscan_nsl3130/include/tcp_connection.hpp
+++ NSL3130_driver/src/roboscan_nsl3130/include/tcp_connection.hpp
@@ -11,7 +11,7 @@ class TcpConnection {
   static const int MARKER_SIZE = 4;
   static const int ACK_BUF_SIZE = 128;
   static constexpr const char* PORT = "50660";
-  static constexpr const char* HOST = "192.168.1.180"; //10.10.31.180 -> 192.168.1.180
+  static constexpr const char* HOST = "192.168.0.220";
   static constexpr const char* END_MARKER = "\xff\xff\x55\xaa";
   static constexpr const char* START_MARKER = "\xff\xff\xaa\x55";
 
